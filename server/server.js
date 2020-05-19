4200d14b8ca539b6018e61ccdf530336cc33bb0b
Wiley Debs <wdebs91@gmail.com>
2020-05-19 17:00:36 -0400

    pointed express to index

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const server = express();
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 server.get('/', (req, res) => {
-  res.send("HELLO");
+  res.sendFile(path.join(path.resolve(__dirname, '..') + '/index.html'));
 });
 
 server.listen(port, hostname, () =>{
