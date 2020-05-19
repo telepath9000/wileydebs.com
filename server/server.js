61057008e38254e434277ed717ebc2f7894419e2
Wiley Debs <wdebs91@gmail.com>
2020-05-19 16:25:37 -0400

    changed to localhost

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const server = express();
 
-const hostname = '206.189.170.144';
+const hostname = '127.0.0.1';
 const port = 8080;
 
 server.get('/', (req, res) => {
   res.send("HELLO");
 });
 
-server.listen(port, hostname);
+server.listen(port, hostname, () =>{
+  console.log(`started server at ${hostname}:${port}`);
+});
