ad7158a3ccebc93339fcca73223eaefaa6e33311
Wiley Debs <wdebs91@gmail.com>
2020-06-09 15:23:32 -0400

    moved sources to subfolder

--- client/index.js
+++ client/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from 'react-route-dom';
 
-import App from './src/App';
+import App from './src/main/App';
 
 ReactDOM.render(<App />, document.getElementById("root"));
